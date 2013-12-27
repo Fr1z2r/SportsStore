f47b6cae4ed3050922c7a4674b5c99c78dae0d82
Fr1z2r <fr1z2rt@gmail.com>
2013-12-27 15:48:41 +0600

    Added fake product repository

--- SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
+++ SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Moq;
 using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Infrastructure
 {
@@ -24,7 +29,16 @@ namespace SportsStore.WebUI.Infrastructure
 
         private void AddBindings()
         {
-            
+            var mock=new Mock<IProductRepository>();
+
+            mock.Setup(m => m.Products).Returns(new List<Product>
+            {
+                new Product {Name = "Football", Price = 25},
+                new Product {Name = "Surf board", Price = 179},
+                new Product {Name = "Running shoes", Price = 95}
+            }.AsQueryable());
+
+            ninjectKernel.Bind<IProductRepository>().ToConstant(mock.Object);
         }
     }
 }
